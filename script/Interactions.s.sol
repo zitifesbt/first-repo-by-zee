543af5d0b1fb3a7a86def04944f188f64dd1521e
zitifesbt otg <robotegbulu@gmail.com>
2025-02-16 12:08:53 +0000

    our first commit!

---
+++ script/Interactions.s.sol
@@ -0,0 +1,52 @@
+// fund
+// withdraw 
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("FundMe funded with %s", SEND_VALUE);
+    }
+
+    function run() external {
+        // DeployFundMe deployFundMe = new DeployFundMe();
+        // fundMe = deployFundMe.run();
+        // vm.deal(USER, STARTING_BALANCE);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe", 
+            block.chainid
+            );
+        vm.startBroadcast();
+        fundFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+}
+
+contract WithdrawFundMe is Script { 
+   function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        // DeployFundMe deployFundMe = new DeployFundMe();
+        // fundMe = deployFundMe.run();
+        // vm.deal(USER, STARTING_BALANCE);
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe", 
+            block.chainid
+            );
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
